1ca1c4a82fad9fa595b91d4f46e186a43ae298c6
Mikhail Dushechkin <mishan92097@gmail.com>
2023-05-04 16:58:37 +0300

    fix: hotfix

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const userRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const { CodeError } = require('./errors/errorCode');
+const NotFoundError = require('./errors/NotFoundError');
 const { login, createUser } = require('./controllers/users');
 const { loginValidation, userValidation } = require('./middlewares/validate');
 const auth = require('./middlewares/auth');
@@ -13,21 +13,21 @@ const auth = require('./middlewares/auth');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(helmet());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet());
 
 app.use(cookieParser());
 
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
 
-app.use('/', auth, userRouter);
+app.use('/', auth, usersRouter);
 app.use('/', auth, cardsRouter);
 
-app.use('*', (req, res) => {
-  res.status(CodeError.NOT_FOUND).send({ message: 'Страница не найдена' });
+app.use('*', () => {
+  throw new NotFoundError('Ресурс не найден');
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
